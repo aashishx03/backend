8f0d01be0098b1b4f746f2d3df90de2d808659a1
Aashish <aashishdubey620@gmail.com>
2023-11-04 06:27:49 +0530

    Remove node_modules from version control

--- natours/app.js
+++ natours/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.json()); // This is the Middleware
+
 const tours = JSON.parse(
   fs.readFileSync(
     `${__dirname}/dev-data/data/tours-simple.json`,
@@ -22,6 +24,26 @@ app.get("/api/v1/tours", (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: tours,
+        },
+      });
+    }
+  );
 });
 
 app.listen(PORT, () => {
