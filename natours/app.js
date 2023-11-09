a8d14b6f4f3a22c668a7fc199cf8a4f83ba54923
Aashish <aashishdubey620@gmail.com>
2023-11-09 08:05:54 +0530

    implemented MVC pattern

--- natours/app.js
+++ natours/app.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-const PORT = 3000;
+const userRouter = require("./routes/userRoutes");
+const tourRouter = require("./routes/tourRoutes");
 
 // 1) MIDDLEWARES
 
@@ -22,159 +22,14 @@ app.use((req, res, next) => {
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    "utf-8",
-    () => {
-      console.log("File is read");
-    }
-  )
-);
-
-// 2) ROUTE HANDELERS
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: "sucess",
-    requestedAt: req.requestTime,
-    result: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  // console.log(id);
-  // console.log(tours.length);
-  if (id < tours.length) {
-    const tour = tours.find((el) => el.id === id);
-    res.status(200).json({
-      status: "sucess",
-      data: {
-        tour,
-      },
-    });
-  } else {
-    res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-};
-
-const createTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: tours,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated Tour Here>",
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "success",
-    message: "This route is not defined yet",
-    data: {
-      user: "There you go here's Complete users data",
-    },
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: "success",
-    message: "This route is not defined yet",
-    data: {
-      user: "This is the data for one User",
-    },
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not defined yet",
-  });
-};
-
 // 3) ROUTES
-
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-tourRouter.route("/").get(getAllTours).post(createTour);
-
-tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route("/").get(getAllUsers).post(createUser);
-
-userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
-// 4) CREATING SERVER
+module.exports = app;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// 4) START SERVER
 
 /////////////////////////////////////
 /////
---
+++ natours/controllers/userController.js
@@ -0,0 +1,40 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    message: "This route is not defined yet",
+    data: {
+      user: "There you go here's Complete users data",
+    },
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "success",
+    message: "This route is not defined yet",
+    data: {
+      user: "This is the data for one User",
+    },
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
